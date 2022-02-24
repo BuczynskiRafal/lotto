2340d6cbf33a210723f00e3e65d5f7671f6e383d
BuczynskiRafal <buczynski.rafal2@gmail.com>
2022-02-24 08:29:48 +0100

    lotto game update

--- main.py
+++ main.py
@@ -5,38 +5,43 @@ the cost of draws and the time needed
 to hit 6 in the lotto.
 """
 import random
-from typing import List
+from typing import Set
 
 
 WEEKS_OF_YEAR = 52
 GAME_COST = 3
 
 
-def get_six_random_numbers() -> List[int]:
+def get_six_random_numbers() -> Set[int]:
     """Draw 6 numbers.
 
     :return: list: Sorted list of six numbers.
     """
-    return sorted(random.sample(range(1, 50), k=6))
+    return set(random.sample(range(1, 50), k=6))
 
 
-def how_many_draws_to_hit_six() -> str:
-    """Calculate how many times you have to play to hit 6 lottery numbers.
+def how_many_draws_to_hit_six() -> int:
+    """Calculate how many times you have to play to hit 6 lottery numbers,
+     always playing with the same numbers.
 
     :return:
     str: The number of draws, the cost of tickets and the time needed to win are given in years.
     """
     counter = 1
+    cross_number = get_six_random_numbers()
     while True:
-        cross_number = get_six_random_numbers()
         lotto_draw = get_six_random_numbers()
         if not cross_number == lotto_draw:
             counter += 1
         else:
-            return f"Ilość losowań: {counter}, " \
-                   f"koszt wszytskich losowań: {counter * 3} pln, " \
-                   f"lata potrzebne żeby trafić 6: {counter/WEEKS_OF_YEAR:.0f} lat"
+            return counter
 
 
 if __name__ == '__main__':
-    print(how_many_draws_to_hit_six())
+    print("Skreślam liczby i obliczam ile razy należy wykonać losowanie "
+          "aby trafić szóstkę stosujac przeudolosowe liczby "
+          "(zakładając że obstawiane liczny się nie zmieniają).")
+    count = how_many_draws_to_hit_six()
+    print(f"Ilość losowań: {count:,}, "
+          f"koszt wszytskich losowań: {count * 3:,} pln, "
+          f"lata potrzebne żeby trafić 6: {count/WEEKS_OF_YEAR:.0f} lat")
